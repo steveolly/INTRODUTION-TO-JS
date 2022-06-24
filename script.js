e1dc10c7ad3b5a5fb2fdfa0d36d0a7462dcf0742
steveolly <ajibolastephen1@gmail.com>
2022-06-24 17:41:33 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+
+  const firstName = "Stephen";
+  const lastName = "Ajibola";
+  const height = "5.5 ft";
+  const country = "Nigeria";
+
+// shown on the screen.
+document.getElementById('fname').innerHTML = 'My first name is ' + firstName + '.'
+document.getElementById('lname').innerHTML = 'My last name is ' + lastName + '.'
+document.getElementById('height').innerHTML = 'I am ' + height + ' tall.'
+document.getElementById('country').innerHTML = 'I am from ' + country + '.'
+
+//shown on at the console page.
+console.log(firstName);
+console.log(lastName);
+console.log(height);
+console.log(country);
